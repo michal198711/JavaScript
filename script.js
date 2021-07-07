228b2bd9e94d4ebe3b04411e0d5ffaa0eeed8e54
Michał <59646682+michal198711@users.noreply.github.com>
2021-07-07 14:23:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const projects = [
+	{
+		name: 'CountdownTimer',
+
+	},
+	{
+		name: 'quiz',
+
+	}, {
+		name: 'recipe',
+
+	},
+	{
+		name: 'weather',
+
+	},
+	{
+		name: 'ToDo',
+
+	},
+
+];
+
+const list = document.getElementById('list');
+
+projects.forEach(({ name}, i) => {
+	const listItem = document.createElement('li');
+
+	listItem.innerHTML = `
+		<a href="/${name}/index.html">
+			<img src="/${name}/screen.jpg" alt="${name}" />
+			<p>${i + 1}. ${formatProjectName(name)}</p>
+		</a>
+
+		<div class="links-container">
+			<a href="/${name}/index.html" class="blue">
+				<i class="fas fa-eye"></i>
+			</a>
+		<button class="btn-grad">test</button>
+		</div>
+	`;
+
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
