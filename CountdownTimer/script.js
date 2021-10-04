bcc94d5f663712cc39a195b6624abe1b36bc35f5
Michał <59646682+michal198711@users.noreply.github.com>
2021-10-04 23:27:28 +0200

    Update script.js

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -7,7 +7,7 @@ const secondsEl = document.getElementById("seconds");
 
 
 function countdown() {
-    const countDownDate = new Date("Aug 27, 2021 19:00:00").getTime();
+    const countDownDate = new Date("Dec, 2021 23:59:59").getTime();
     const now = new Date().getTime();
     
     const distance  =countDownDate- now  ;
