e775552e821ab928821f4b8e21805b60a7fe0344
Michał <59646682+michal198711@users.noreply.github.com>
2022-01-23 23:03:11 +0100

    Add files via upload

---
+++ textStatistics/script.js
@@ -0,0 +1,12 @@
+let textArea = document.getElementsByTagName("textarea")[0];
+textArea.addEventListener("input", onChange);
+function onChange(e){
+    const data = textArea.value;
+    const numCharacters = data.length;
+    const numWords = data.replace(/[\r\n]/g, "").split(" ").length - 1;
+    const numSentences = data.split(".").length - 1;
+
+    document.getElementById("num-characters").innerHTML = numCharacters;
+    document.getElementById("num-words").innerHTML = numWords;
+    document.getElementById("num-sentences").innerHTML = numSentences;
+}
