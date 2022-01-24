f151af2b406ff90117edca8ea43d11a48c5492f9
Michał <59646682+michal198711@users.noreply.github.com>
2022-01-24 22:43:27 +0100

    Update script.js

--- textStatistics/script.js
+++ textStatistics/script.js
@@ -4,7 +4,7 @@ function onChange(e){
     const data = textArea.value;
     const numCharacters = data.length;
     const numWords = data.replace(/[\r\n]/g, "").split(" ").length - 1;
-    const numSentences = data.split(".").length - 1;
+    const numSentences = data.split(".").length + data.split("!").length - 1;
 
     document.getElementById("num-characters").innerHTML = numCharacters;
     document.getElementById("num-words").innerHTML = numWords;
