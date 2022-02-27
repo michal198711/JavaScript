f52cb8775b7a12e8c5e671067514423815c8abbe
Michał <59646682+michal198711@users.noreply.github.com>
2022-02-27 18:27:37 +0100

    Update script.js

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -10,12 +10,15 @@ function countdown() {
     const countDownDate = new Date("Dec, 2021 23:59:59").getTime();
     const now = new Date().getTime();
     
-    const distance  =countDownDate- now  ;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)+1);
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+    const distance  = now countDownDate  ;
+ //  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const days = distance.getDay()
+ // const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)+1);
+    const hours = distance.getHours()
+ //  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const minutes = distance.minutes()
+ // const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const seconds = distance.seconds()
     daysEl.innerHTML=formatTime(days);
     hoursEl.innerHTML=formatTime(hours);
     minsEl.innerHTML=formatTime(minutes);
