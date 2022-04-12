83f6b1f1c0905ae915247a3b7cc648a06f3b28d8
Michał <59646682+michal198711@users.noreply.github.com>
2022-04-12 21:05:31 +0200

    Add files via upload

---
+++ checkbox/app.js
@@ -0,0 +1,32 @@
+const PositiveMess = () => <p>Zapraszamy</p>
+const NegativeMess = () => <p>Nie zapraszamy</p>
+class CheckboxAgeConfirmation extends React.Component {
+    state = {
+        isChecked: false,
+    }
+    handleChackboxChange = () => {
+        this.setState({
+            isChecked: !this.state.isChecked
+        })
+    }
+
+    displayMessage = () => {
+        if (this.state.isChecked) {
+            return <PositiveMess />
+        } else {
+            return <NegativeMess />
+        }
+    }
+    render() {
+        console.log(this.state.isChecked)
+        return(
+            <>
+    <h1> Kup bilet</h1>
+    <input type="checkbox" id="ageConfirmation" onChange = {this.handleChackboxChange} checked = {this.state.isChecked}/>
+    <label htmlFor="ageConfirmation">Mam wiecej niz 18 lat</label>
+    {displayMessage()}
+      </>  )
+
+        }
+}
+ReactDOM.render(<CheckboxAgeConfirmation />, document.getElementById('app'));
