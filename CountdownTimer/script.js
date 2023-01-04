8297bd7b3e9e790a6f6d5e9130c3cba4c96397af
Michał <59646682+michal198711@users.noreply.github.com>
2023-01-04 22:12:34 +0100

    Update script.js

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const countDownDate = "1 Jan 2023";
+const countDownDate = "1 Jan 2024";
 
 function countdown() {
     const newCountDownDate = new Date(countDownDate);
