77887c24c1b39cd75882f1c50c87b69754854b91
Michał <59646682+michal198711@users.noreply.github.com>
2023-01-10 22:16:20 +0100

    Add files via upload
    add DOM

--- stokrotka/script.js
+++ stokrotka/script.js
@@ -1,16 +1,29 @@
-function phoneNumber(daisyNumber) {
-    let wholeNumber=""
-  if (daisyNumber < 500) {
-    partOne = "515110";
-    partTwo = daisyNumber.toString();
-    return wholeNumber = partOne + partTwo;
-  } else if (daisyNumber > 500 && daisyNumber < 1000) {
-    partOne = "515070 ";
-    partTwo = daisyNumber.toString();
-    return wholeNumber = partOne + partTwo;
-  } else {
-    partOne = "57133";
-    partTwo = daisyNumber.toString();
-    return wholeNumber = partOne + partTwo;
-  }
+
+/*function alertNumber(val) {
+alert("Prawdopodobny numer to: " + phoneNumber(val));
+}*/
+
+function showNumber(val){
+document.getElementById("demo").innerHTML = "Prawdopodobny numer: "+ phoneNumber(val);
+}
+  function phoneNumber(daisyNumber) {
+let wholeNumber=""
+if (daisyNumber < 100) {
+partOne = "515-110-";
+partTwo = "0"+daisyNumber.toString();
+return wholeNumber = partOne + partTwo;}
+else if  (daisyNumber >100 && daisyNumber < 500) {
+partOne = "515-110-";
+partTwo = daisyNumber.toString();
+return wholeNumber = partOne + partTwo;
+} else if (daisyNumber > 500 && daisyNumber < 1000) {
+partOne = "515-070-";
+partTwo = daisyNumber.toString();
+return wholeNumber = partOne + partTwo;
+} else {
+partOne = "571-331-";
+partTwo = (daisyNumber.toString()).slice(1);
+return wholeNumber = partOne + partTwo;
+}
+console.log(wholeNumber)
 }
