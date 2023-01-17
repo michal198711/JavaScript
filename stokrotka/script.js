373fe8568172ce51e106326bc85c5ae699506e5d
Michał <59646682+michal198711@users.noreply.github.com>
2023-01-17 09:55:04 +0100

    Update script.js

--- stokrotka/script.js
+++ stokrotka/script.js
@@ -17,7 +17,7 @@ partOne = "515-110-";
 partTwo = daisyNumber.toString();
 return wholeNumber = partOne + partTwo;
 } else if (daisyNumber > 500 && daisyNumber < 1000) {
-partOne = "515-070-";
+partOne = "519-070-";
 partTwo = daisyNumber.toString();
 return wholeNumber = partOne + partTwo;
 } else {
