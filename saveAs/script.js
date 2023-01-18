8003f130242a34c29abbf31585879572003ecc3f
Michał <59646682+michal198711@users.noreply.github.com>
2023-01-18 21:54:22 +0100

    Add files via upload

---
+++ saveAs/script.js
@@ -0,0 +1,14 @@
+const textarea = document.getElementById("textarea")
+const fileNameInput = document.getElementById("file-name")
+const selectMenu = document.getElementById("select-menu")
+const saveBtn =document.getElementById("save-btn")
+
+saveBtn.addEventListener("click", () => {
+
+    const blob = new Blob([textarea.value + ], {type: selectMenu.value});
+    const fileUrl = URL.createObjectURL(blob)
+    const link  = document.createElement("a");
+    link.download= fileNameInput.value;
+    link.href = fileUrl;
+    link.click();
+})
